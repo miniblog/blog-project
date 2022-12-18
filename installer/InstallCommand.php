ef2e1cbfcac9f55e869cfb9b2c9fb6a711c91dc3
Daniel Bettles <danbettles@yahoo.co.uk>
2022-12-18 12:44:12 +0000

    Added missing comment.

--- installer/InstallCommand.php
+++ installer/InstallCommand.php
@@ -49,6 +49,9 @@ class InstallCommand
         echo "{$message}\n";
     }
 
+    /**
+     * See https://gist.github.com/egmontkob/eb114294efbcd5adb1944c9f3cb5feda
+     */
     private function createHyperlink(string $url, string $label): string
     {
         $osc = "\033]";
